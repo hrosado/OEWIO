8f69bc4c59a7d6a03e98387a65044739a7fbd169
hommy.rosado <hommy.rosado@gmail.com>
2020-08-02 18:10:16 -0400

    Testing Product controller prior to new layout changes.

---
+++ OEWIO/Controllers/ProdAPIController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using OEWIO.Models;
+
+namespace OEWIO.Controllers
+{
+    public class ProdAPIController : ApiController
+    {
+        private OEWIOEntities db = new OEWIOEntities();
+
+        // GET: api/ProdAPI
+        public IQueryable<OEWIOProduct> GetOEWIOProducts()
+        {
+            return db.OEWIOProducts;
+        }
+
+        // GET: api/ProdAPI/5
+        [ResponseType(typeof(OEWIOProduct))]
+        public IHttpActionResult GetOEWIOProduct(Guid id)
+        {
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            if (oEWIOProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(oEWIOProduct);
+        }
+
+        // PUT: api/ProdAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOEWIOProduct(Guid id, OEWIOProduct oEWIOProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != oEWIOProduct.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(oEWIOProduct).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OEWIOProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ProdAPI
+        [ResponseType(typeof(OEWIOProduct))]
+        public IHttpActionResult PostOEWIOProduct(OEWIOProduct oEWIOProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.OEWIOProducts.Add(oEWIOProduct);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (OEWIOProductExists(oEWIOProduct.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = oEWIOProduct.ID }, oEWIOProduct);
+        }
+
+        // DELETE: api/ProdAPI/5
+        [ResponseType(typeof(OEWIOProduct))]
+        public IHttpActionResult DeleteOEWIOProduct(Guid id)
+        {
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            if (oEWIOProduct == null)
+            {
+                return NotFound();
+            }
+
+            db.OEWIOProducts.Remove(oEWIOProduct);
+            db.SaveChanges();
+
+            return Ok(oEWIOProduct);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OEWIOProductExists(Guid id)
+        {
+            return db.OEWIOProducts.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ OEWIO/Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OEWIO.Models;
+
+namespace OEWIO.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private OEWIOEntities db = new OEWIOEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var oEWIOProducts = db.OEWIOProducts.Include(o => o.OEWIOAuthor);
+            return View(oEWIOProducts.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            if (oEWIOProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oEWIOProduct);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.OEWIOAuthors, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ProductDate,AuthorID,Created,Modified")] OEWIOProduct oEWIOProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                oEWIOProduct.ID = Guid.NewGuid();
+                db.OEWIOProducts.Add(oEWIOProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.OEWIOAuthors, "ID", "FirstName", oEWIOProduct.AuthorID);
+            return View(oEWIOProduct);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            if (oEWIOProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.OEWIOAuthors, "ID", "FirstName", oEWIOProduct.AuthorID);
+            return View(oEWIOProduct);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ProductDate,AuthorID,Created,Modified")] OEWIOProduct oEWIOProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(oEWIOProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.OEWIOAuthors, "ID", "FirstName", oEWIOProduct.AuthorID);
+            return View(oEWIOProduct);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            if (oEWIOProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oEWIOProduct);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            OEWIOProduct oEWIOProduct = db.OEWIOProducts.Find(id);
+            db.OEWIOProducts.Remove(oEWIOProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
